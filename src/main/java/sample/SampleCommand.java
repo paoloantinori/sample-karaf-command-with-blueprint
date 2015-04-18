9047428ead2c2ce8cec26a681ce89c7fe1ea0e72
Paolo Antinori <pantinor@redhat.com>
2015-04-18 12:46:14 +0200

    Initial commit

---
+++ src/main/java/sample/SampleCommand.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class SampleCommand {
+
+	
+	public void first() {
+		System.out.println("first");
+	}	
+
+	public void second() {
+		System.out.println("second");
+	}
+	
+	public void hello(String name) {
+		System.out.println("hello " + name );
+	}
+	
+}
